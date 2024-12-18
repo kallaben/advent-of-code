1e84f2e3a3a79edbb1710520401216040b0199b1
kallaben <github@beamli.com>
2024-12-18 17:05:56 +0100

    Day 5, puzzle 2

---
+++ day5/day5_2.ts
@@ -0,0 +1,43 @@
+import { pageInput, ruleInput } from "./input";
+
+const rules = ruleInput
+  .split("\n")
+  .map((rule) => rule.split("|").map((num) => Number(num)));
+
+const sortMap = rules.reduce((map, [small, large]) => {
+  if (!map.has(small)) {
+    map.set(small, new Map());
+  }
+
+  if (!map.has(large)) {
+    map.set(large, new Map());
+  }
+
+  const innerMapSmall = map.get(small);
+  const innerMapLarge = map.get(large);
+
+  innerMapSmall!.set(large, 1);
+  innerMapLarge!.set(small, -1);
+  return map;
+}, new Map<number, Map<number, -1 | 1>>());
+
+const pages = pageInput
+  .split("\n")
+  .map((line) => line.split(",").map((num) => Number(num)));
+
+const unsortedPages = pages.filter(
+  (page) =>
+    JSON.stringify(page) !==
+    JSON.stringify([...page].sort((a, b) => sortMap.get(b)?.get(a) ?? 0)),
+);
+
+const newlySortedPages = unsortedPages.map((page) =>
+  [...page].sort((a, b) => sortMap.get(b)?.get(a) ?? 0),
+);
+
+const sumOfMiddleNumbers = newlySortedPages.reduce(
+  (sum, page) => sum + page[Math.floor((page.length - 1) / 2)],
+  0,
+);
+
+console.log(sumOfMiddleNumbers);
